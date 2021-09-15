8bcccc7616c2526495e1649d9595f0b72c66a99a
Vitaopb <victor.lieuthier@gmail.com>
2021-09-15 10:35:38 -0300

    Project final

---
+++ interfaces/IRepositorio.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+
+namespace AvanadeFlix.interfaces
+{
+    public interface IRepositorio<T>
+    {
+         List<T> Lista();
+         T RetornaPorId(int id);
+         void Insere(T objeto);
+         void Exclui(int id);
+         void Atualiza(int id, T objeto);
+         int ProximoId();
+    }
+}
